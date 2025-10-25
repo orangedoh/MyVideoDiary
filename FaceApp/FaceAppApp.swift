00c0b123c138e84ecc2f64018f09cd5a8172f211
Rimma Davletova <508350@student.saxion.nl>
2025-10-25 19:05:27 +0200

    Initial Commit

---
+++ FaceApp/FaceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceAppApp.swift
+//  FaceApp
+//
+//  Created by Римма Давлетова on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct FaceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
